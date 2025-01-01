a402ffda39f6d76a4fec7c60dbf7d4878be56274
Evan <evan_maus@berkeley.edu>
2025-01-01 07:51:58 -0800

    change of blog page styling and completion

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,4 +1,3 @@
-// app/blog/[slug]/page.js
 import RenderMdx from "@/src/components/Blog/RenderMdx"
 import siteMetadata from "@/src/utils/siteMetaData"
 import { blogs } from "@/.velite/generated"
@@ -8,23 +7,14 @@ import Image from "next/image"
 import { notFound } from "next/navigation"
 
 export async function generateStaticParams() {
-  // Get all MDX blog slugs
   const mdxSlugs = blogs.map((blog) => ({ slug: blog.slug }))
-  
-  // Get all Contentful blog slugs
   const contentfulPosts = await getBlogPosts()
-  const contentfulSlugs = contentfulPosts.map((post) => ({ 
-    slug: post.fields.slug 
-  }))
-  
-  // Combine both sets of slugs
+  const contentfulSlugs = contentfulPosts.map((post) => ({ slug: post.fields.slug }))
   return [...mdxSlugs, ...contentfulSlugs]
 }
 
 export async function generateMetadata({ params }) {
   const { slug } = params
-  
-  // Check MDX blogs first
   const mdxBlog = blogs.find((blog) => blog.slug === slug)
   if (mdxBlog) {
     const publishedAt = new Date(mdxBlog.publishedAt).toISOString()
@@ -38,7 +28,6 @@ export async function generateMetadata({ params }) {
       url: img.includes("http") ? img : siteMetadata.siteUrl + img
     }))
     const authors = mdxBlog?.author ? [mdxBlog.author] : siteMetadata.author
-    
     return {
       title: mdxBlog.title,
       description: mdxBlog.description,
@@ -55,13 +44,9 @@ export async function generateMetadata({ params }) {
       },
     }
   }
-  
-  // If not MDX, check Contentful
+
   const contentfulPosts = await getBlogPosts()
-  const contentfulBlog = contentfulPosts.find(
-    (post) => post.fields.slug === slug
-  )
-  
+  const contentfulBlog = contentfulPosts.find((post) => post.fields.slug === slug)
   if (contentfulBlog) {
     return {
       title: contentfulBlog.fields.title,
@@ -76,15 +61,13 @@ export async function generateMetadata({ params }) {
       },
     }
   }
-  
   return null
 }
 
 export default async function BlogPage({ params }) {
   const { slug } = params
-  
-  // Try to find MDX blog first
   const mdxBlog = blogs.find((blog) => blog.slug === slug)
+  
   if (mdxBlog) {
     let imageList = [siteMetadata.socialBanner]
     if (mdxBlog.image) {
@@ -101,20 +84,15 @@ export default async function BlogPage({ params }) {
       image: imageList,
       datePublished: new Date(mdxBlog.publishedAt).toISOString(),
       dateModified: new Date(mdxBlog.updatedAt || mdxBlog.publishedAt).toISOString(),
-      author: [
-        {
-          "@type": "Person",
-          name: mdxBlog?.author ? [mdxBlog.author] : siteMetadata.author,
-        },
-      ],
+      author: [{
+        "@type": "Person",
+        name: mdxBlog?.author ? [mdxBlog.author] : siteMetadata.author,
+      }],
     }
     
     return (
       <>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
         <article>
           <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
             <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -144,12 +122,9 @@ export default async function BlogPage({ params }) {
       </>
     )
   }
-  
-  // If not MDX, try to find Contentful blog
+
   const contentfulPosts = await getBlogPosts()
-  const contentfulBlog = contentfulPosts.find(
-    (post) => post.fields.slug === slug
-  )
+  const contentfulBlog = contentfulPosts.find((post) => post.fields.slug === slug)
   
   if (contentfulBlog) {
     const jsonLd = {
@@ -159,27 +134,33 @@ export default async function BlogPage({ params }) {
       description: contentfulBlog.fields.description || contentfulBlog.fields.title,
       datePublished: new Date(contentfulBlog.sys.createdAt).toISOString(),
       dateModified: new Date(contentfulBlog.sys.updatedAt).toISOString(),
-      author: [
-        {
-          "@type": "Person",
-          name: siteMetadata.author,
-        },
-      ],
+      author: [{
+        "@type": "Person",
+        name: siteMetadata.author,
+      }],
     }
     
     return (
       <>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
         <article>
-          <div className="mb-8 text-center relative w-full">
-            <h1 className="text-4xl font-bold mb-4">{contentfulBlog.fields.title}</h1>
-            {contentfulBlog.fields.description && (
-              <p className="text-xl text-gray-600 dark:text-gray-300">
-                {contentfulBlog.fields.description}
-              </p>
+          <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
+            <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <h1 className="inline-block mt-6 font-semibold capitalize text-light text-2xl md:text-3xl lg:text-5xl !leading-normal relative w-5/6">
+                {contentfulBlog.fields.title}
+              </h1>
+            </div>
+            <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-dark/60 dark:bg-dark/40" />
+            {contentfulBlog.fields.image && (
+              <Image
+                src={`https:${contentfulBlog.fields.image.fields.file.url}`}
+                alt={contentfulBlog.fields.image.fields.title || contentfulBlog.fields.title}
+                width={contentfulBlog.fields.image.fields.file.details.image.width}
+                height={contentfulBlog.fields.image.fields.file.details.image.height}
+                className="aspect-square w-full h-full object-cover object-center"
+                priority
+                sizes="100vw"
+              />
             )}
           </div>
           <div className="flex items-center justify-center px-5 md:px-10">
